1a615ff62a3f4a0e77309686143b8579073e7125
Yahdi Indrawan <55028341+yahdiindrawan@users.noreply.github.com>
2020-11-10 11:09:10 +0700

    Update PreprocessingJurnal.py

--- PreprocessingJurnal.py
+++ PreprocessingJurnal.py
@@ -1,27 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import csv
 import string
-# import Sastrawi
-# from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-# from nltk.tokenize import word_tokenize
-# from nltk.corpus import stopwords
-# from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactorydassfgfog,
+import Sastrawi
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 
-# In[4]:
-
-
-filedataolah = 'DataS.csv'
+filedataolah = 'DatasetJurnal.csv'
 f = open(filedataolah,'r')
 reader = csv.reader(f)
-
-
-# In[5]:
 
 
 def casefolding(judul, abstrak, keyword, label):
@@ -31,10 +19,6 @@ def casefolding(judul, abstrak, keyword, label):
     label = label.lower()
     return(judul, abstrak, keyword, label)
 
-
-# In[17]:
-
-
 hasil_case_folding = []
 for data in reader:
     hasil = casefolding(data[1],data[2],data[3],data[4])
@@ -42,14 +26,7 @@ for data in reader:
 print(hasil_case_folding)   
 
 
-# In[16]:
-
-
 print(hasil_case_folding)
-
-
-# In[8]:
-
 
 def remove_punctuation(judul, abstrak, keyword, label):
     judul = judul.translate(str.maketrans('','',string.punctuation)).strip()
@@ -57,9 +34,6 @@ def remove_punctuation(judul, abstrak, keyword, label):
     keyword = keyword.translate(str.maketrans('','',string.punctuation)).strip()
     label = label.translate(str.maketrans('','',string.punctuation)).strip()
     return(judul, abstrak, keyword, label)
-
-
-# In[9]:
 
 
 hasil_remove_punctuation = []
@@ -71,14 +45,7 @@ for data in range(len(hasil_case_folding)):
     hasil_remove_punctuation.append(hasil)
 print(hasil_remove_punctuation)
 
-
-# In[11]:
-
-
 print (hasil_remove_punctuation)
-
-
-# In[12]:
 
 
 def tokenize(judul,abstrak,keyword,label):
@@ -87,10 +54,6 @@ def tokenize(judul,abstrak,keyword,label):
     keyword = word_tokenize(keyword)
     label = word_tokenize(label)
     return(judul, abstrak, keyword, label)
-
-
-# In[13]:
-
 
 hasil_tokenize = []
 for data in range(len(hasil_remove_punctuation)):    
@@ -101,15 +64,7 @@ for data in range(len(hasil_remove_punctuation)):
     hasil_tokenize.append(hasil)
 print(hasil_tokenize)
 
-
-# In[14]:
-
-
 print(hasil_tokenize)
-
-
-# In[15]:
-
 
 factory = StopWordRemoverFactory()
 stopword = factory.create_stop_word_remover()
@@ -140,9 +95,6 @@ def stopwords(judul,abstrak,keyword,label):
     return(hasil_judul, hasil_abstrak, hasil_keyword, hasil_label) 
 
 
-# In[16]:
-
-
 hasil_stopwords = []
 for data in range(len(hasil_tokenize)):    
     hasil = stopwords(hasil_tokenize[data][0],
@@ -151,9 +103,6 @@ for data in range(len(hasil_tokenize)):
                       hasil_tokenize[data][3]) 
     hasil_stopwords.append(hasil)
 print(hasil_stopwords)
-
-
-# In[17]:
 
 
 factory = StemmerFactory()
@@ -185,9 +134,6 @@ def stemming(judul,abstrak,keyword,label):
     return(hasil_judul, hasil_abstrak, hasil_keyword, hasil_label)
 
 
-# In[18]:
-
-
 hasil_stemming = []  
 for data in range(len(hasil_stopwords)):    
     hasil = stemming (hasil_stopwords[data][0],
@@ -196,39 +142,6 @@ for data in range(len(hasil_stopwords)):
                       hasil_stopwords[data][3]) 
     hasil_stemming.append(hasil)
 print(hasil_stemming)
-
-
-# In[23]:
-
-
-import pandas as pd 
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
-
-
-# In[28]:
-
-
-def bagOfWords(judul): 
-    hasil_judul = [] 
-    for i in judul:
-        bgOfWords = bagOfWords.split(i)
-        if bgOfWords !='':
-            hasil_judul.append(bgOfWords) 
-   
-    return(hasil_judul)
-
-
-# In[29]:
-
-
-hasil_bagOfWords = []  
-for data in range(len(hasil_stemming)):    
-    hasil = bagOfWords (hasil_stemming[data][0]) 
-    hasil_bagOfWords.append(hasil)
-print(hasil_bagOfWords)
-
-
-# In[160]:
 
 
 hasil_preprocessing = hasil_stemming
@@ -242,31 +155,6 @@ json_preprocessing = {
 }
 
 
-# In[161]:
-
-
-json_preprocessing
-
-
-# In[162]:
-
-
 import json
 with open('hasil_preprocessing.json', 'w') as json_file:
   json.dump(json_preprocessing, json_file)
-
-
-# In[18]:
-
-
-#cara import pandas, tidak diload dalam proses ini
-import pandas as pd
-
-judul = []
-abstrak = []
-keyword = []
-label = []
-
-data_dict = {'JUDUL':judul, 'ABSTRAK':abstrak, 'KEYWORD':keyword, 'LABEL':label}
-df = pd.DataFrame(data_dict,columns=['JUDUL','ABSTRAK','KEYWORD','LABEL'])
-
